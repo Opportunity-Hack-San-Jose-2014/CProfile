cc7db3e0c03c850762b968695c5455143b5a58b5
UUppuluri <UUppuluri@macbookpro-fd88.minoltana.minolta.com>
2014-10-13 17:41:36 -0700

    Removed unused files

--- topNavbar.php
+++ topNavbar.php
@@ -1,5 +1,16 @@
-           <!-- /.navbar-header -->
-
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="home.php">CProfile.org</a>
+            </div>
+            <!-- /.navbar-header -->
+ 
             <ul class="nav navbar-top-links navbar-right">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
